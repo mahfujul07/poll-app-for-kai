c53f10d2c0c0d0496215188650cfa2f1271beecd
mahfujul07 <mahfujulhuq07@gmail.com>
2022-08-25 22:06:29 +0400

    init

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Not_found from "./Pages/Not_found";
 import Footer from "./Pages/Footer";
 import Auth from "./Pages/Auth";
 import UserProvider from "./firebase/UserProvider";
-// import PrivateRoute from "./Pages/PrivateRoute";
+import PrivateRoute from "./Pages/PrivateRoute";
 
 function App() {
   return (
@@ -13,11 +13,11 @@ function App() {
       <UserProvider>
         <Router>
           <Switch>
-            <Route exact path="/" component={Home} />
+            <PrivateRoute exact path="/" component={Home} />
             <Route exact path="/auth" component={Auth} />
             <Route exact path="/not_found" component={Not_found} />
-            <Route exact path="/:id" component={Poll} />
-            <Route path="*" exact={false} component={Not_found} />
+            <PrivateRoute exact path="/:id" component={Poll} />
+            <Route path="*" exact={true} component={Not_found} />
           </Switch>
           <Footer />
         </Router>
